0645cee7681b4b25bcd348bb61aac097b868f610
xcd0 <xcd0@xcd0.com>
2021-05-02 01:30:26 +0900

    Reverseとかdimとかsusとかの妥協実装

--- keyboards/Moon50/keymaps/default/icDefineCode.h
+++ keyboards/Moon50/keymaps/default/icDefineCode.h
@@ -12,10 +12,10 @@ enum note { // n:ナチュラル s:シャープ b:フラット
 };
 
 enum codeType {
-	MEJAR = 0,          // 0 Mejar
+	MAJOR = 0,          // 0 Major
 	MINOR,              // 1 Minor
 	SEVENS,             // 2 Sevens
-	MEJARSEVENS,        // 3 MejarSevens
+	MAJORSEVENS,        // 3 MajarSevens
 	MINORSEVENS,        // 4 MinorSevens
 	MINORMEJORSEVENS,   // 5 MinorMejorSevens
 	DIMINISH,           // 6 Diminish
@@ -26,10 +26,10 @@ enum codeType {
 
 // memo : 普通ギターのCの6つの弦は E1, C2, E2, G2, C3, E3
 // uint8_t ic[9][6] = {
-//   { 0, 4, 7, 12, 16, 19 }, // 0 Mejar
+//   { 0, 4, 7, 12, 16, 19 }, // 0 Major
 //   { 0, 3, 7, 12, 15, 19 }, // 1 Minor
 //   { 0, 4, 7, 10, 12, 16 }, // 2 Sevens
-//   { 0, 4, 7, 11, 12, 16 }, // 3 MejarSevens
+//   { 0, 4, 7, 11, 12, 16 }, // 3 MajorSevens
 //   { 0, 3, 7, 10, 12, 15 }, // 4 MinorSevens
 //   { 0, 3, 7, 11, 12, 15 }, // 5 MinorMejorSevens
 //   { 0, 3, 6, 12, 15, 18 }, // 6 Diminish
@@ -38,12 +38,16 @@ enum codeType {
 // };
 
 typedef struct {
-	char      reverse          ; // = false;             // insta code の ~ 相当
-	uint8_t   baseNote         ; // = 0;                 // 音 icDefineCode.hのenum note
+	uint8_t   baseNote         ; // = 0;                 // 音 icDefineCode.hのenum note // noteOffsetとの組み合わせでスケールが決まる
 	int       octaveOffset     ; // = 1;                 // オクターブオフセット  min=-1 max=9
 	int       noteOffset       ; // = 0;                 // 半音単位でのオフセット いわゆる変調
 	uint8_t   codeType         ; // = 0;                 // Majorとか
-	uint8_t icCodeOffsetArray[9][6]; // = icCodeOffsetArray; // 和音のそれぞれの音baseからのoffset配列へのポインタ
+	long      codeStatus       ; // = 0;                 // 
+	char      isForsed         ; // = false;             // dimやsusなどの種類変更キーが押されているか
+	char      isReversed       ; // = false;             // ~ Reverseが押されているか Major <-> Minor
+	char      isSharped        ; // = false;             // # が押されているか 半音あげる
+	uint8_t   pressedForseCode ; // = 0;                 // 押されているコード種類変更キー enum codeTypeが入るのを想定
+	uint8_t   icCodeOffsetArray[9][6]; // = icCodeOffsetArray; // 和音のそれぞれの音baseからのoffset配列へのポインタ
 }IcData;
 
 // 和音を設定する
